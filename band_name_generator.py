9bc75728667dde8533488b601215c5ff376df57c
ov1faruk <gfo.ovi@gmail.com>
2022-07-10 03:55:55 +0600

    bandnamegenerator

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+# creates a greeting by simply printing a line
+print("Welcome to the *Band Name Generator*")
+# some more line
+print("#####################################")
+#takes an input 
+city_name= input("What is name of the city you grew in?\n")
+#takes another input 
+pet_name=input("Name of your pet?\n")
+#concatinates two strings & finally outputs the band name 
+print("The name of your band should be: "+pet_name+" "+city_name)
