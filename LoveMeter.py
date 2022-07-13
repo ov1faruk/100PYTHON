08317362f0a36e43c540cadba22d9c5738b4235e
ov1faruk <gfo.ovi@gmail.com>
2022-07-13 07:11:20 +0600

    update color

--- LoveMeter.py
+++ LoveMeter.py
@@ -1,13 +1,24 @@
-# 🚨 Don't change the code below 👇
-print("Welcome to the Love Calculator!")
+print("###############################\n")
+print("Welcome to the Love Calculator!\n")
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 name=name1+name2
 name=name.lower()
-# print(name)
+
 L=name.count("l")
 O=name.count("o")
 V=name.count("v")
@@ -22,7 +33,7 @@ true=T+R+U+E
 score=f"{true}{love}"
 score=int(score)
 if score<10 or score>90:
-    print(f"your score is {score},you go together like coke and mentos.")
+    print(f"your score is {score},"+color.BOLD+"you go together like "+color.RED+"coke "+color.END+"and"+color.BLUE+" mentos."+color.END)
 elif score>40 and score<50:
     print(f"your score is {score},you are alright together.")
 else:
